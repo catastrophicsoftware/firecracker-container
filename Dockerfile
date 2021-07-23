19fe22d6dce965a8a4e1e55aaeddb577a86529d0
cssoftware <0@1.null>
2021-07-23 13:19:05 -0700

    -cleaned up entrypoint -added /vm as workdir for container image -changed entrypoint for container image -added barebones ci pipeline

--- Dockerfile
+++ Dockerfile
@@ -1,11 +1,12 @@
 FROM alpine:3.9
+WORKDIR /vm
+
 RUN apk update && apk add iputils iproute2 wget htop openssh-client
 
 #Install firecracker
 RUN cd /tmp && wget https://github.com/firecracker-microvm/firecracker/releases/download/v0.23.4/firecracker-v0.23.4-x86_64.tgz
 RUN tar -zxf /tmp/firecracker-v0.23.4-x86_64.tgz && cd release-v0.23.4 && chmod +x firecracker-v0.23.4-x86_64 && mv firecracker-v0.23.4-x86_64 /usr/bin/firecracker
 
-RUN mkdir /vm
 COPY entrypoint.sh /
 
-CMD sh /entrypoint.sh
+ENTRYPOINT ["sh", "/entrypoint.sh"]
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+MICROVM_IP='172.20.0.2'
 
 echo "Creating bridge interface"
 ip link add name br0 type bridge
@@ -8,7 +9,6 @@ ip link set dev br0 up
 echo "Done!"
 
 echo "Configuring NAT"
-#sysctl -w net.ipv4.ip_forward=1
 iptables --table nat --append POSTROUTING --out-interface eth0 -j MASQUERADE
 iptables --insert FORWARD --in-interface br0 -j ACCEPT
 echo "Done!"
@@ -21,9 +21,8 @@ echo "Done!"
 
 echo "Adding default port forwarding rule for ssh on 23"
 iptables -A PREROUTING -t nat -i eth0 -p tcp --dport 23 -j DNAT --to 172.20.0.2:23 #TODO: automate this
-#in production situations, we don't know what the internal address of the MicroVM will be.
 echo "Done!"
 
 echo "Launching MicroVM!"
-firecracker --no-api --config-file /vm/config.json
+firecracker --no-api --config-file config.json
 echo "MicroVM Terminated!"
